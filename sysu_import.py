3b344251070be1f6746f9ac6a4b3c59426312ea9
airmelt <4irmelt@gmail.com>
2022-05-25 10:59:06 +0800

    add filter_file function

--- sysu_import.py
+++ sysu_import.py
@@ -91,5 +91,65 @@ def add_csv_bom(input_file: str) -> None:
         f.write('\ufeff' + content)
 
 
+def filter_file(input_file: str) -> None:
+    df1 = pd.read_excel(input_file, sheet_name='dgvTable', dtype=str)
+    df = pd.DataFrame(columns=['凭证日期', '凭证字', '凭证编号', '摘要', '科目编号', '科目名称',
+                               '借金额', '贷金额'])
+    new_row = pd.DataFrame(index=['0'], columns=['凭证日期', '凭证字', '凭证编号', '摘要', '科目编号',
+                                                 '科目名称', '借金额', '贷金额'])
+    num_set = set()
+    subject_set = {'10010101', '10020103', '1002010401', '1002010602', '100201250101', '101101',
+                   '101102', '101103', '10020114'}
+    fifty = 0
+    hundred = 0
+    fh = 0
+    th = 0
+    for i in range(df1.shape[0]):
+        num = df1.iloc[i, 2]
+        subject_num = df1.iloc[i, 9]
+        if num in num_set:
+            continue
+        if subject_num not in subject_set:
+            continue
+        num_set.add(num)
+        new_row['凭证日期'] = df1.iloc[i, 0]
+        new_row['凭证字'] = df1.iloc[i, 1]
+        new_row['凭证编号'] = df1.iloc[i, 2]
+        new_row['摘要'] = df1.iloc[i, 6]
+        new_row['科目编号'] = df1.iloc[i, 9]
+        new_row['科目名称'] = df1.iloc[i, 10]
+        new_row['借金额'] = df1.iloc[i, 15]
+        new_row['贷金额'] = float(df1.iloc[i, 16])
+        if float(df1.iloc[i, 16]) > 500000.00:
+            fifty += 1
+        if float(df1.iloc[i, 16]) > 1000000.00:
+            hundred += 1
+        if float(df1.iloc[i, 16]) > 1500000.00:
+            fh += 1
+        if float(df1.iloc[i, 16]) > 2000000.00:
+            th += 1
+        df = df.append(new_row, ignore_index=True)
+    df.sort_values(by=['贷金额'], inplace=True)
+    new_row['凭证编号'] = ''
+    new_row['摘要'] = ''
+    new_row['科目编号'] = ''
+    new_row['科目名称'] = ''
+    new_row['借金额'] = ''
+    new_row['贷金额'] = ''
+    new_row['凭证日期'] = '大于50万'
+    new_row['凭证字'] = str(fifty)
+    df = df.append(new_row, ignore_index=True)
+    new_row['凭证日期'] = '大于100万'
+    new_row['凭证字'] = str(hundred)
+    df = df.append(new_row, ignore_index=True)
+    new_row['凭证日期'] = '大于150万'
+    new_row['凭证字'] = str(fh)
+    df = df.append(new_row, ignore_index=True)
+    new_row['凭证日期'] = '大于200万'
+    new_row['凭证字'] = str(th)
+    df = df.append(new_row, ignore_index=True)
+    df.to_excel('过滤' + input_file, index=False, encoding='utf-8')
+
+
 if __name__ == '__main__':
     import_file('')
